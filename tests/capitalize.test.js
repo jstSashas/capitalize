8353072560728ca2f8a597684ae1eafa817e0471
Александра  Казакова <jst_sashas@MacBook-Pro-Aleksandra.local>
2023-11-06 11:23:04 +0300

    add project files

---
+++ tests/capitalize.test.js
@@ -0,0 +1,9 @@
+import { strict as assert } from 'node:assert';
+
+import capitalize from '../src/capitalize.js';
+
+assert.equal(capitalize('hello'), 'Hello');
+
+assert.equal(capitalize(''), '');
+
+console.log('Все тесты пройдены!');
